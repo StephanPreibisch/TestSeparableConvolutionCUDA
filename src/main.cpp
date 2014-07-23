1380efa5eacd558585d67469b77a1b734d144247
Stephan Preibisch <stephan.preibisch@gmx.de>
2014-07-23 12:52:36 -0400

    added test for 3d convolution

--- src/main.cpp
+++ src/main.cpp
@@ -161,6 +161,168 @@ void test2d()
     }
 }
 
+void test3d()
+{
+    StopWatchInterface *hTimer = NULL;
+
+    float
+    *h_Kernel,
+    *h_Input,
+    *h_Buffer,
+    *h_OutputCPU,
+    *h_OutputGPU;
+
+    float
+    *d_Input,
+    *d_Output,
+    *d_Buffer;
+
+    const int imageW = 512;
+    const int imageH = 512;
+    const int imageD = 512;
+    const int iterations = 16;
+
+    printf("Image Width x Height x Depth = %i x %ix %i\n\n", imageW, imageH, imageD );
+    printf("Allocating and initializing host arrays...\n");
+    h_Kernel    = (float *)malloc(KERNEL_LENGTH * sizeof(float));
+    h_Input     = (float *)malloc(imageW * imageH * imageD * sizeof(float));
+    h_Buffer    = (float *)malloc(imageW * imageH * imageD * sizeof(float));
+    h_OutputCPU = (float *)malloc(imageW * imageH * imageD * sizeof(float));
+    h_OutputGPU = (float *)malloc(imageW * imageH * imageD * sizeof(float));
+    srand(200);
+
+    for (unsigned int i = 0; i < KERNEL_LENGTH; i++)
+    {
+        h_Kernel[i] = (float)(rand() % 16);
+    }
+
+    for (unsigned i = 0; i < imageW * imageH * imageD; i++)
+    {
+        h_Input[i] = (float)(rand() % 16);
+    }
+
+    sdkCreateTimer(&hTimer);
+
+    printf("Allocating and initializing CUDA arrays...\n");
+    checkCudaErrors(cudaMalloc((void **)&d_Input,   imageW * imageH * imageD * sizeof(float)));
+    checkCudaErrors(cudaMalloc((void **)&d_Output,  imageW * imageH * imageD * sizeof(float)));
+    checkCudaErrors(cudaMalloc((void **)&d_Buffer , imageW * imageH * imageD * sizeof(float)));
+
+    setConvolutionKernel(h_Kernel);
+    checkCudaErrors(cudaMemcpy(d_Input, h_Input, imageW * imageH * imageD * sizeof(float), cudaMemcpyHostToDevice));
+
+    printf("Running GPU convolution3d (%u identical iterations)...\n\n", iterations);
+
+    for (int i = -1; i < iterations; i++)
+    {
+        //i == -1 -- warmup iteration
+        if (i == 0)
+        {
+            checkCudaErrors(cudaDeviceSynchronize());
+            sdkResetTimer(&hTimer);
+            sdkStartTimer(&hTimer);
+        }
+
+        convolution3dRowsGPU(
+            d_Buffer,
+            d_Input,
+            imageW,
+            imageH,
+            imageD
+        );
+
+        convolution3dColumnsGPU(
+            d_Output,
+            d_Buffer,
+            imageW,
+            imageH,
+            imageD
+        );
+
+        convolution3dDepthGPU(
+            d_Output,
+            d_Buffer,
+            imageW,
+            imageH,
+            imageD
+        );
+    }
+
+    checkCudaErrors(cudaDeviceSynchronize());
+    sdkStopTimer(&hTimer);
+    double gpuTime = 0.001 * sdkGetTimerValue(&hTimer) / (double)iterations;
+    printf("convolutionSeparable3d, Throughput = %.4f MPixels/sec, Time = %.5f s, Size = %u Pixels, NumDevsUsed = %i, Workgroup = %u\n",
+           (1.0e-6 * (double)(imageW * imageH * imageD)/ gpuTime), gpuTime, (imageW * imageH * imageD), 1, 0);
+
+    printf("\nReading back GPU results...\n\n");
+    checkCudaErrors(cudaMemcpy(h_OutputGPU, d_Output, imageW * imageH * sizeof(float), cudaMemcpyDeviceToHost));
+
+    printf("Checking the results...\n");
+    printf(" ...running convolution3dRowCPU()\n");
+    convolution3dRowCPU(
+        h_Buffer,
+        h_Input,
+        h_Kernel,
+        imageW,
+        imageH,
+        imageD,
+        KERNEL_RADIUS
+    );
+
+    printf(" ...running convolution3dColumnCPU()\n");
+    convolution3dColumnCPU(
+        h_OutputCPU,
+        h_Buffer,
+        h_Kernel,
+        imageW,
+        imageH,
+        imageD,
+        KERNEL_RADIUS
+    );
+
+    printf(" ...running convolution3dDepthCPU()\n");
+    convolution3dDepthCPU(
+        h_OutputCPU,
+        h_Buffer,
+        h_Kernel,
+        imageW,
+        imageH,
+        imageD,
+        KERNEL_RADIUS
+    );
+
+    printf(" ...comparing the results\n");
+    double sum = 0, delta = 0;
+
+    for (unsigned i = 0; i < imageW * imageH * imageD; i++)
+    {
+        delta += (h_OutputGPU[i] - h_OutputCPU[i]) * (h_OutputGPU[i] - h_OutputCPU[i]);
+        sum   += h_OutputCPU[i] * h_OutputCPU[i];
+    }
+
+    double L2norm = sqrt(delta / sum);
+    printf(" ...Relative L2 norm 3d: %E\n\n", L2norm);
+    printf("Shutting down...\n");
+
+
+    checkCudaErrors(cudaFree(d_Buffer));
+    checkCudaErrors(cudaFree(d_Output));
+    checkCudaErrors(cudaFree(d_Input));
+    free(h_OutputGPU);
+    free(h_OutputCPU);
+    free(h_Buffer);
+    free(h_Input);
+    free(h_Kernel);
+
+    sdkDeleteTimer(&hTimer);
+
+    if (L2norm > 1e-6)
+    {
+        printf("3d-Test failed!\n");
+        exit(EXIT_FAILURE);
+    }
+}
+
 ////////////////////////////////////////////////////////////////////////////////
 // Main program
 ////////////////////////////////////////////////////////////////////////////////
